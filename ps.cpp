2ba8cc66be5618cf62e235c45b2a0a3738b85e43
Your Name <you@example.com>
2018-04-24 14:57:56 +0800

    testrand

--- ps.cpp
+++ ps.cpp
@@ -171,14 +171,16 @@ int main(int argc, const char * argv[])
     {
         for (int j = 0; j < K; j++)
         {
-            P[i][j] =  ((double)rand() / RAND_MAX ) / sqrt(K);
+            //P[i][j] =  ((double)rand() / RAND_MAX ) / sqrt(K);
+            P[i][j] = drand48();
         }
     }
     for (int i = 0; i < K; i++)
     {
         for (int j = 0; j < M; j++)
         {
-            Q[i][j] = ((double)rand() / RAND_MAX) / sqrt(K);
+            //Q[i][j] = ((double)rand() / RAND_MAX) / sqrt(K);
+            Q[i][j] = drand48();
         }
     }
     for (int i = 0; i < WORKER_NUM; i++)

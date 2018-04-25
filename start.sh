c6b1ef8b182492132bfa323e1f97ce4c51dde839
Your Name <you@example.com>
2018-04-25 13:33:56 +0800

    aa

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@
 g++  -g  -std=c++11  -pthread ps.cpp -o ps
 g++  -g  -std=c++11  -pthread worker.cpp -o worker
 g++  -g  -std=c++11  -pthread rworker.cpp -o rworker
+g++  -g  -std=c++11  -pthread genSeq.cpp -o genSeq
+g++  -g  -std=c++11  -pthread PartitionMap.cpp -o PartitionMap

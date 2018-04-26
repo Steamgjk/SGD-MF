676c5da5a02f66711ec9c187887e289f3be6240c
Your Name <you@example.com>
2018-04-26 19:43:13 +0800

    fix

--- worker.cpp
+++ worker.cpp
@@ -379,7 +379,7 @@ double CalcRMSE(map<long, double>& RTestMap, Block & minP, Block & minQ)
             //sum += P[row_idx][k] * Q[k][col_idx];
             if (row_idx * K + k > minP.eles.size() || col_idx * K + k > minQ.eles.size() )
             {
-                printf("Psz %ld  idx %ld  Qsz %ld  idx %ld\n", minP.eles.size(), row_idx * K + k ,   minQ.eles.size(), col_idx * K + k );
+                printf("Psz %ld  idx %ld  Qsz %ld  idx %ld  real_hash_idx %ld row_idx %ld col_idx %ld\n", minP.eles.size(), row_idx * K + k ,   minQ.eles.size(), col_idx * K + k, real_hash_idx, row_idx, col_idx );
                 getchar();
             }
             sum += minP.eles[row_idx * K + k] * minQ.eles[col_idx * K + k];

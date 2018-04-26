7ceb33cce1841c6113d35ff74399678bea86be57
Your Name <you@example.com>
2018-04-26 17:40:25 +0800

    ring

--- start.sh
+++ start.sh
@@ -4,3 +4,4 @@ g++  -g  -std=c++11  -pthread worker.cpp -o worker
 g++  -g  -std=c++11  -pthread rworker.cpp -o rworker
 g++  -g  -std=c++11  -pthread genSeq.cpp -o genSeq
 g++  -g  -std=c++11  -pthread PartitionMap.cpp -o PartitionMap
+g++  -g  -std=c++11  -pthread genRingSeq.cpp -o genRingSeq

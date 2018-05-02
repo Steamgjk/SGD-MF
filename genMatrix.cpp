dd7c916a2b614009978dd1151aea4dba59ad4c2e
Your Name <you@example.com>
2018-05-02 20:49:34 +0800

    Matrix

--- genMatrix.cpp
+++ genMatrix.cpp
@@ -11,11 +11,67 @@ using namespace std;
 #define K 10
 double P[N][K];
 double Q[K][N];
+#define TD_NUM 16
+void work_func(int td)
+{
+	printf("Thread td %d\n", td );
+	double sum = 0;
+	long hash_id = 0;
+	srand(time(0));
+	int r = 0;
+	char fn[100];
+	sprintf(fn, "./data/TrainingMap-%d", td);
+	ofstream Hofs(fn, ios::trunc);
+	sprintf(fn, "./data/TestingMap-%d", td);
+	ofstream Tofs(fn, ios::trunc);
+
+	long cnt = 0;
+	long train_cnt = 0;
+	long test_cnt = 0;
+
+	long row_len = N / TD_NUM;
+	long row_idx = td * row_len;
+
+	for (long i = row_idx; i < row_idx + row_len; i++)
+	{
+		for (long j = 0; j < M; j++)
+		{
+			r = rand() % 10000;
+			if (r < 20)
+			{
+				hash_id = i * M + j;
+				sum = 0;
+				for (int k = 0; k < K; k++)
+				{
+					sum += P[i][k] * Q[k][j];
+				}
+				Hofs << hash_id << " " << sum << endl;
+				train_cnt++;
+				r = rand() % 10000;
+				if (r < 20)
+				{
+					Tofs << hash_id << " " << sum << endl;
+					test_cnt++;
+				}
+			}
+			cnt++;
+			if (cnt % 1000000 == 0)
+			{
+				printf("[%d]:cnt = %ld train_cnt=%ld test_cnt=%ld\n", td, cnt, train_cnt, test_cnt);
+			}
+		}
+	}
+	printf("[%d]  finished\n", td );
+
+}
 int main()
 {
 	srand(time(0));
-	ofstream Pofs("./Pmtx.txt", ios::trunc);
-	ofstream Qofs("./Qmtx.txt", ios::trunc);
+	char fn[100];
+	sprintf(fn, "./data/Pmtx");
+	ofstream Pofs(fn, ios::trunc);
+	sprintf(fn, "./data/Qmtx");
+	ofstream Qofs(fn, ios::trunc);
 	std::default_random_engine e; //引擎
 	std::normal_distribution<double> n(1, 0.1); //均值, 方差
 	for (int i = 0; i < N; i++)
@@ -37,45 +93,17 @@ int main()
 		Qofs << endl;
 	}
 	cout << "finish P and Q" << endl;
-	double sum = 0;
-	long hash_id = 0;
-	srand(time(0));
-	int r = 0;
-	ofstream Hofs("./TrainingMap.txt", ios::trunc);
-	ofstream Tofs("./TestingMap.txt", ios::trunc);
-	long cnt = 0;
-	long train_cnt = 0;
-	long test_cnt = 0;
-	for (long i = 0; i < N; i++)
+
+
+	for (int td = 0; td < TD_NUM; td++)
 	{
-		for (long j = 0; j < M; j++)
-		{
-			hash_id = i * M + j;
-			sum = 0;
-			for (int k = 0; k < K; k++)
-			{
-				sum += P[i][k] * Q[k][j];
-			}
-			r = rand() % 1000;
-			if (r < 20)
-			{
-				Hofs << hash_id << " " << sum << endl;
-				train_cnt++;
-			}
-			r = rand() % 10000000;
-			if (r < 20)
-			{
-				Tofs << hash_id << " " << sum << endl;
-				test_cnt++;
-			}
-			cnt++;
-			if (cnt % 1000000 == 0)
-			{
-				printf("cnt = %ld train_cnt=%ld test_cnt=%ld\n", cnt, train_cnt, test_cnt);
-			}
-		}
+		std::thread worker_thread(work_func, td);
+		worker_thread.detach();
 	}
-
-
+	while (true)
+	{
+		std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+		printf("sleep\n");
+	}
 
 }

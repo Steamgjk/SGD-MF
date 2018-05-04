6f97bfd5f26fee9457867b1d10f559bcd2d49f34
Your Name <you@example.com>
2018-05-04 19:08:15 +0800

    dd

--- genNewMatrix.cpp
+++ genNewMatrix.cpp
@@ -60,7 +60,11 @@ void WriteTrain()
 	long sz = (M / 8);
 	sz = sz * N / 8;
 	int file_idx = 0;
-
+	int ccc = 10;
+	while (ccc--)
+	{
+		printf("train_head=%ld train_tail=%ld\n", train_head, train_head );
+	}
 	while (train_head < train_tail)
 	{
 		file_idx = TrainHash[train_head] / sz;
@@ -97,69 +101,10 @@ void WriteTest()
 		test_head++;
 		if (test_head % 1000 == 0)
 		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 			printf("test_head = %ld\n", test_head );
 		}
 	}
-}
-void work_func(int td)
-{
-	printf("Thread td %d\n", td );
-	double sum = 0;
-	long hash_id = 0;
-	srand(time(0));
-	int r = 0;
-	char fn[100];
-	sprintf(fn, "./data/TrainingMap-%d", td);
-	ofstream Hofs(fn, ios::trunc);
-	sprintf(fn, "./data/TestingMap-%d", td);
-	ofstream Tofs(fn, ios::trunc);
-	int row_idx = td / DIM_SIZE;
-	int col_idx = td % DIM_SIZE;
-	long row_len = N / DIM_SIZE;
-	long col_len = M / DIM_SIZE;
-	long row_sta_idx = row_idx * row_len;
-	long col_sta_idx = col_idx * col_len;
-
-	long cnt = 0;
-	long train_cnt = 0;
-	long test_cnt = 0;
-	for (long i = row_sta_idx; i < row_sta_idx + row_len; i++)
-	{
-		for (long j = col_sta_idx; j < col_sta_idx + col_len; j++)
-		{
-			//20M
-			r = rand() % 10000000;
-			if (r < 200)
-			{
-				hash_id = i * M + j;
-				sum = subM[i % 100][j % 100];
-
-
-				//Hofs << hash_id << " " << sum << endl;
-				TrainHash[train_tail] =  hash_id;
-				TrainVal[train_tail] = sum;
-				train_tail++;
-				//train_cnt++;
-				//2M
-				//r = rand() % 1000;
-				if (r < 20)
-				{
-					//Tofs << hash_id << " " << sum << endl;
-					//test_cnt++;
-					TestHash[test_tail] = hash_id;
-					TestVal[test_tail] = sum;
-					test_tail++;
-				}
-			}
-			cnt++;
-			if (cnt % 10000000 == 0)
-			{
-				//printf("[%d]：:cnt = %ld train_cnt=%ld test_cnt=%ld\n", td, cnt, train_tail, test_tail);
-			}
-		}
-	}
-	printf("[%d]  finished\n", td );
-
 }
 //double val[10] = {0.000755, 0.025004, 0.304611, 1.365174, 2.250791, 1.365174, 0.304611, 0.025004, 0.000755, 0.000008};
 
@@ -230,8 +175,7 @@ int main()
 				//r = rand() % 1000;
 				if (r < 20)
 				{
-					//Tofs << hash_id << " " << sum << endl;
-					//test_cnt++;
+
 					TestHash[test_tail] = hash_id;
 					TestVal[test_tail] = sum;
 					test_tail++;
@@ -245,44 +189,7 @@ int main()
 		}
 	}
 
-	/*
-		srand(time(0));
-		char fn[100];
-		sprintf(fn, "./data/Pmtx");
-		ofstream Pofs(fn, ios::trunc);
-		sprintf(fn, "./data/Qmtx");
-		ofstream Qofs(fn, ios::trunc);
 
-		long unit_len = N / K;
-		for (int i = 0; i < N; i++)
-		{
-			long offset = i / unit_len;
-			for (int j = 0; j < K; j++)
-			{
-				P[i][j] = val[(j + offset) % K];
-				//Pofs << P[i][j] << " ";
-			}
-			Pofs << endl;
-		}
-		for (int i = 0; i < K; i++)
-		{
-			for (int j = 0; j < M; j++)
-			{
-				long offset = j / unit_len;
-				Q[i][j] = val[(i + offset) % K];
-				//Qofs << Q[i][j] << " ";
-			}
-			Qofs << endl;
-		}
-		cout << "finish P and Q" << endl;
-	**/
-	/*
-		for (int td = 0; td < TD_NUM; td++)
-		{
-			std::thread worker_thread(work_func, td);
-			worker_thread.detach();
-		}
-		**/
 	while (true)
 	{
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000));

c5f8600c4b4fd393675d383145c23a7a7152b14c
Your Name <you@example.com>
2018-05-07 11:32:01 +0800

    dd

--- rworker_en.cpp
+++ rworker_en.cpp
@@ -257,6 +257,7 @@ int main(int argc, const char * argv[])
                 printf("to recv\n");
                 std::this_thread::sleep_for(std::chrono::milliseconds(1000));
             }
+            getchar();
 
         }
 

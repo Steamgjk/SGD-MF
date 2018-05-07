48259f6cf471bebd2622505686318d0792492ccd
Your Name <you@example.com>
2018-05-07 16:54:18 +0800

    ddd

--- rworker_en.cpp
+++ rworker_en.cpp
@@ -418,15 +418,16 @@ void readData(int data_thread_id)
     long hash_id;
     double rate;
     long cnt = 0;
+    /*
     while (1 == 1)
     {
         printf("INi head_idx=%d  to_send_tail=%d\n", head_idx, to_send_tail );
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-
+    **/
     while (head_idx < to_send_tail)
     {
-        printf("head_idx=%d  to_send_tail=%d\n", head_idx, to_send_tail );
+        printf("head_idx=%d  to_send_tail=%d tail_idx=%d\n", head_idx, to_send_tail, tail_idx );
         if (tail_idx >= QU_LEN)
         {
             break;
@@ -483,6 +484,7 @@ void readData(int data_thread_id)
         TestMaps[row][col].clear();
         head_idx++;
     }
+    printf("Exit read data\n");
 
 }
 void getBlockRates(map<long, double>& BlockMap, int block_id)

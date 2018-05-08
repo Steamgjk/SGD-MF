0bb5c72c66c941d1f0a3e1bc0aa47daac3b77acf
Your Name <you@example.com>
2018-05-08 17:42:22 +0800

    dd

--- ps.cpp
+++ ps.cpp
@@ -200,7 +200,7 @@ int main(int argc, const char * argv[])
         {
             //P[i][j] =  ((double)rand() / RAND_MAX ) / sqrt(K);
             //P[i][j] = drand48() / 1.7;
-            P[i][j] = drand48();
+            P[i][j] = drand48() * 0.4;
             //P[i][j] = drand48() * 0.38;
             //P[i][j] =  ((double)(rand() % 100) ) / 400 ;
 
@@ -213,7 +213,7 @@ int main(int argc, const char * argv[])
         {
             //Q[i][j] = ((double)rand() / RAND_MAX) / sqrt(K);
             //Q[i][j] = drand48() / 1.7;
-            Q[i][j] = drand48();
+            Q[i][j] = drand48() * 0.4;
             //Q[i][j] = drand48() * 0.38 ;
             //Q[i][j] =  ((double)(rand() % 100) ) / 400 ;
 

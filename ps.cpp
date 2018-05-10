1ef40b6275df9442e9c2553a1e52b725a5f2869d
Your Name <you@example.com>
2018-05-10 22:46:47 +0800

    comparison

--- ps.cpp
+++ ps.cpp
@@ -258,6 +258,10 @@ int main(int argc, const char * argv[])
 
         random_shuffle(worker_pidx, worker_pidx + WORKER_NUM); //迭代器
         random_shuffle(worker_qidx, worker_qidx + WORKER_NUM); //迭代器
+        for (int i = 0; i < WORKER_NUM; i++)
+        {
+            printf("%d  [%d:%d]\n", i, worker_pidx[i], worker_qidx[i] );
+        }
 
         for (int i = 0; i < WORKER_NUM; i++)
         {

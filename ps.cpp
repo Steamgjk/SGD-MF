7d7d072054a1cf739ff08139238252ef32e20d57
Your Name <you@example.com>
2018-05-10 22:50:49 +0800

    comparison

--- ps.cpp
+++ ps.cpp
@@ -239,25 +239,26 @@ int main(int argc, const char * argv[])
     {
         partitionP(WORKER_NUM, Pblocks);
         partitionQ(WORKER_NUM, Qblocks);
-        /*
+
         for (int i = 0; i < WORKER_NUM; i++)
         {
             worker_qidx[i] = (worker_qidx[i] + 1) % 4;
         }
-        **/
+
         //getchar();
         srand(time(0));
         bool ret = false;
 
         int per_num = 1;
-
-        for (int i = 0; i < WORKER_NUM; i++)
-        {
-            worker_pidx[i] = worker_qidx[i] = i;
-        }
+        /*
+                for (int i = 0; i < WORKER_NUM; i++)
+                {
+                    worker_pidx[i] = worker_qidx[i] = i;
+                }
 
         random_shuffle(worker_pidx, worker_pidx + WORKER_NUM); //迭代器
         random_shuffle(worker_qidx, worker_qidx + WORKER_NUM); //迭代器
+         **/
         for (int i = 0; i < WORKER_NUM; i++)
         {
             printf("%d  [%d:%d]\n", i, worker_pidx[i], worker_qidx[i] );

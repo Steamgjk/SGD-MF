0a201012d6414c254a1814706217759b575b9208
Your Name <you@example.com>
2018-05-11 15:06:13 +0800

    comparison

--- CalcRMSE.cpp
+++ CalcRMSE.cpp
@@ -76,8 +76,10 @@ double CalcRMSE()
     int negative_cnt = 0;
     double sss = 0;
     double avg = 0;
+    printf("hehere\n");
     for (iter = TestMap.begin(); iter != TestMap.end(); iter++)
     {
+        printf("hehe11\n");
         long real_hash_idx = iter->first;
         long row_idx = real_hash_idx / M;
         long col_idx = real_hash_idx % M;

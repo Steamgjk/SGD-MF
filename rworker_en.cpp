f766d8302f75de05f224e9aff9d2308b482957ff
Your Name <you@example.com>
2018-05-14 15:01:26 +0800

    rima-Jumbo-time-test

--- rworker_en.cpp
+++ rworker_en.cpp
@@ -401,7 +401,7 @@ void CalcUpdt(int td_id)
         {
 
 
-            int times_thresh = 1000;
+            int times_thresh = 500;
             int row_sta_idx = Pblocks[p_block_idx].sta_idx;
             int col_sta_idx = Qblocks[q_block_idx].sta_idx;
             size_t rtsz;

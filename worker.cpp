f55406b3c345169ea2d42eee20be03d5b1780314
Your Name <you@example.com>
2018-05-15 11:33:39 +0800

    dsgd-movielen-1000-iter-time-test-debug

--- worker.cpp
+++ worker.cpp
@@ -462,7 +462,7 @@ void CalcUpdt(int td_id)
                     Pblock.eles[i * K + k] += yita * (error * oldQ[j * K + k] - theta * oldP[i * K + k]);
                     if (Pblock.eles[i * K + k] + 1 == Pblock.eles[i * K + k] - 1)
                     {
-                        printf("p %d q %d  error =%lf i=%d j=%d k=%d\n", p_block_idx, q_block_idx, error, i, j, k );
+                        printf("p %d q %d  error =%lf i=%d j=%d k=%d rand_idx=%d vale=%lf\n", p_block_idx, q_block_idx, error, i, j, k, rand_idx,  rates_for_col_threads[p_block_idx][q_block_idx][td_id][rand_idx] );
                         getchar();
                     }
                 }
@@ -477,6 +477,7 @@ void CalcUpdt(int td_id)
                     continue;
                 }
                 error = rates_for_col_threads[p_block_idx][q_block_idx][td_id][rand_idx];
+
                 for (int k = 0; k < K; ++k)
                 {
                     error -= oldP[i * K + k] * oldQ[j * K + k];
@@ -486,7 +487,7 @@ void CalcUpdt(int td_id)
                     Qblock.eles[j * K + k] += yita * (error * oldP[i * K + k] - theta * oldQ[j * K + k]);
                     if (Qblock.eles[j * K + k] + 1 == Qblock.eles[j * K + k] - 1)
                     {
-                        printf("p %d q %d  error =%lf i=%d j=%d k=%d\n", p_block_idx, q_block_idx, error, i, j, k );
+                        printf("p %d q %d  error =%lf i=%d j=%d k=%d rand_idx=%d vale=%lf\n", p_block_idx, q_block_idx, error, i, j, k, rand_idx,  rates_for_col_threads[p_block_idx][q_block_idx][td_id][rand_idx] );
                         getchar();
 
                     }

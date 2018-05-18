9c714cb04e6517e42ca7e23b8a326c8b81e04f5a
Your Name <you@example.com>
2018-05-19 02:08:21 +0800

    dd

--- client_rdma_op.h
+++ client_rdma_op.h
@@ -11,7 +11,7 @@ int client_pre_post_recv_buffer();
 int client_connect_to_server();
 int client_send_metadata_to_server();
 int client_send_metadata_to_server1(void* send_buf, size_t send_sz);
-int start_remote_write(int len);
+int start_remote_write(size_t len, size_t offset)
 int client_remote_memory_ops();
 int client_disconnect_and_clean();
 

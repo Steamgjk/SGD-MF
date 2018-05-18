7f2b648a0ac7b99d478d8c9994b388707f8e4cee
Your Name <you@example.com>
2018-05-18 15:13:47 +0800

    update make for rdma

---
+++ scp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+scp -r ainet@12.12.10.16:/home/ainet/gjk/SGD-MF/Rtrack/* ./Rtrack/
+scp -r ainet@12.12.10.12:/home/ainet/gjk/SGD-MF/Rtrack/* ./Rtrack/
+scp -r ainet@12.12.10.15:/home/ainet/gjk/SGD-MF/Rtrack/* ./Rtrack/
--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 rm ps worker rworker rworker_en
-g++  -g   ps.cpp -o ps -pthread -std=c++11
-g++  -g   worker.cpp -o worker -pthread -std=c++11
-g++  -g   rworker.cpp -o rworker -pthread -std=c++11
-g++  -g   rworker_en.cpp -o rworker_en -pthread -std=c++11
-g++  -g   genSeq.cpp -o genSeq -pthread -std=c++11
-g++  -g   PartitionMap.cpp -o PartitionMap -pthread -std=c++11
-g++  -g   genRingSeq.cpp -o genRingSeq -pthread -std=c++11
-g++  -g   genMatrix.cpp -o genMatrix -pthread -std=c++11
-g++  -g   genNewMatrix.cpp -o genNewMatrix -pthread -std=c++11
+g++  -g   ps.cpp -o ps -pthread -libverbs -lrdmacm -std=c++11 
+g++  -g   worker.cpp -o worker -pthread -libverbs -lrdmacm -std=c++11
+g++  -g   rworker.cpp -o rworker -pthread -libverbs -lrdmacm -std=c++11
+g++  -g   rworker_en.cpp -o rworker_en -pthread -libverbs -lrdmacm -std=c++11
+g++  -g   genSeq.cpp -o genSeq -pthread -libverbs -lrdmacm -std=c++11
+g++  -g   PartitionMap.cpp -o PartitionMap -pthread -libverbs -lrdmacm -std=c++11
+g++  -g   genRingSeq.cpp -o genRingSeq -pthread -libverbs -lrdmacm -std=c++11
+g++  -g   genMatrix.cpp -o genMatrix -pthread -libverbs -lrdmacm -std=c++11
+g++  -g   genNewMatrix.cpp -o genNewMatrix -pthread -libverbs -lrdmacm -std=c++11
+g++  -g   CalcRMSE.cpp -o CalcRMSE -pthread -libverbs -lrdmacm -std=c++11
+g++  -g   check.cpp -o check -pthread -libverbs -lrdmacm -std=c++11

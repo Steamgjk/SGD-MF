2c2a3e1083185c6456fff779a0c0c0819f81bb2c
Your Name <you@example.com>
2018-05-19 16:48:42 +0800

    test whay

--- ps.cpp
+++ ps.cpp
@@ -737,8 +737,8 @@ void rdma_sendTd(int send_thread_id)
         }
         else
         {
-            printf("cannotsend\n");
-            getchar();
+            //printf("cannotsend\n");
+            //getchar();
         }
     }
 

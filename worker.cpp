0c6ec7124b8056b75d49ea581f557a71d6fcee62
Your Name <you@example.com>
2018-05-20 13:00:22 +0800

    debug-test

--- worker.cpp
+++ worker.cpp
@@ -1061,7 +1061,7 @@ void rdma_sendTd(int send_thread_id)
 }
 void rdma_recvTd(int recv_thread_id)
 {
-    printf("rdma_recv thread_id = %d\n local_ip=%s  local_port=%d\n", recv_thread_id, local_ips[recv_thread_id % WORKER_N_1], local_ports[recv_thread_id]);
+    printf("rdma_recv thread_id = %d\n local_ip-11=%s  local_port-11=%d\n", recv_thread_id, local_ips[recv_thread_id % WORKER_N_1], local_ports[recv_thread_id]);
     server_rdma_op sro;
     int ret = sro.rdma_server_init(local_ips[recv_thread_id % WORKER_N_1], local_ports[recv_thread_id], to_recv_block_mem, MEM_SIZE);
 

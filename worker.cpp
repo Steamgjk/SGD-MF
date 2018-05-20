c651bcbf7a2849fbaecb77806e2b6d6ab795468f
Your Name <you@example.com>
2018-05-20 09:50:45 +0800

    debug

--- worker.cpp
+++ worker.cpp
@@ -1123,6 +1123,7 @@ void rdma_recvTd(int recv_thread_id)
         Qblock.ele_num = qb-> ele_num;
         Qblock.eles.resize(qb->ele_num);
         //printf("recv pele %d qele %d\n", Pblock.ele_num, Qblock.ele_num );
+        printf("[%d]get qblock id=%d  ele_num=%d  isP=%d\n", recv_thread_id,  qb->block_id, qb->ele_num, qb->isP);
         data_eles = (double*)(void*)(to_recv_block_mem + BLOCK_MEM_SZ + struct_sz);
         for (int i = 0; i < Qblock.ele_num; i++)
         {

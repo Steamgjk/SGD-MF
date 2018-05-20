5d77761d09dbd9465e6f24c7f607e39325528b5e
Your Name <you@example.com>
2018-05-21 01:19:33 +0800

    dsgd-rdma

--- worker.cpp
+++ worker.cpp
@@ -1043,10 +1043,10 @@ void rdma_sendTd(int send_thread_id)
             memcpy(real_sta_buf + p_total, &(Qblock), struct_sz);
             memcpy(real_sta_buf + p_total + struct_sz , (char*) & (Qblock.eles[0]), q_data_sz);
             memcpy(real_sta_buf + total_len, &total_len, sizeof(int));
-            /*
-                        ret = cro.start_remote_write(real_total, 0);
-                        printf("[%d]:writer another block success real_total=%ld\n", send_thread_id, real_total);
-            **/
+
+            ret = cro.start_remote_write(real_total, 0);
+            printf("[%d]:writer another block success real_total=%ld\n", send_thread_id, real_total);
+
             *flag = total_len;
             printf("flag=%d\n", (*flag) );
             ret = cro.start_remote_write(sizeof(int), 0);

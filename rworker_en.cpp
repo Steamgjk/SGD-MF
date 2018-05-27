fddf9ba980fd665f9eea4c2b75b06da6adb33675
Your Name <you@example.com>
2018-05-27 20:35:05 +0800

    retest-yahoo-rdma-8-debug

--- rworker_en.cpp
+++ rworker_en.cpp
@@ -100,8 +100,9 @@ double theta = 0.05;
 #define QU_LEN 10000
 
 #define WORKER_THREAD_NUM 30
-#define BLOCK_MEM_SZ (25000000)
-#define MEM_SIZE (BLOCK_MEM_SZ*80)
+long BLOCK_MEM_SZ = (3000000);
+//250M*8
+long MEM_SIZE = (BLOCK_MEM_SZ * 800);
 
 #define WORKER_N_1 4
 #define QP_GROUP 1

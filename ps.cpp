81ca1ed6d70a76a669386e59f8d5e8bc2703fdf7
Your Name <you@example.com>
2018-05-28 14:18:43 +0800

    retest-yahoo-rdma-qp-15

--- ps.cpp
+++ ps.cpp
@@ -94,7 +94,7 @@ struct client_context c_ctx[CAP];
 struct conn_context s_ctx[CAP];
 #endif
 
-#define QP_GROUP 25
+#define QP_GROUP 5
 int send_round_robin_idx[CAP];
 int recv_round_robin_idx[CAP];
 
